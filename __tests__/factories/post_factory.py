86db33b0a2911c8f4b01a9723245f142421ae262
Eros <garotoslixo@gmail.com>
2023-11-15 15:27:52 -0300

    Tests added

---
+++ __tests__/factories/post_factory.py
@@ -0,0 +1,16 @@
+import factory
+from faker import Faker
+from app import db
+from app.models import Post
+
+faker = Faker()
+
+class PostFactory(factory.alchemy.SQLAlchemyModelFactory):
+    class Meta:
+        model = Post
+        sqlalchemy_session = db.session
+        sqlalchemy_session_persistence = "commit"
+    
+    title = faker.paragraph()
+    text = faker.text()
+    published = True
--- __tests__/home_test.py
+++ __tests__/home_test.py
@@ -1,25 +1,42 @@
 from ward import test
-from splinter import Browser
-from app import create_app, db
-from app.models import Post
 from __tests__.fixtures import browser
+from faker import Faker
+from flask import url_for
+from __tests__.factories.post_factory import PostFactory
+
 
 @test('Main page must be online')
 def _(browser=browser):
 
-    browser.visit('/')
+    browser.visit(url_for('home.index'))
 
     assert browser.is_text_present('Hello')
 
 
-@test('Current user acess the main page and sees a post')
+@test('Current user access main page and sees a post')
+def _(browser=browser):
+    post = PostFactory.create()
+
+    browser.visit(url_for('home.index'))
+
+    assert browser.is_text_present(post.title)
+
+
+@test('Current user accesses main page and doesnt see a post')
 def _(browser=browser):
 
-    post = Post(title='Vacations!', published=True, 
-        text='Somebody once told me the world is gonna roll me')
-    db.session.add(post)
-    db.session.commit()
+    browser.visit(url_for('home.index'))
 
-    browser.visit('/')
+    assert browser.is_text_present('Nothing to do here')
 
-    assert browser.is_text_present('Vacations!')
+
+@test('Current user accesses main page and doesnt see unpublished posts')
+def _(browser=browser):
+    post = PostFactory.create(title='Vacations!')
+    drafts = PostFactory.create_batch(2, published=False)
+
+    browser.visit(url_for('home.index'))
+
+    assert browser.is_text_present(post.title)
+    assert browser.is_text_not_present(drafts[0].title)
+    assert browser.is_text_not_present(drafts[1].title)
