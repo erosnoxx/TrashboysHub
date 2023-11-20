14a320778f1d78797489472779f02c96ca57faf9
santjonatas <jonatascaina100@gmail.com>
2023-11-20 13:49:48 -0300

    Revert "ok"
    This reverts commit a8a5ec30993aa887703a93dfa0fe7550e452cf74.

--- migrations/versions/39720cae17e2_.py
+++ migrations/versions/39720cae17e2_.py
@@ -1,8 +1,8 @@
 """empty message
 
-Revision ID: 9e8969e799bd
+Revision ID: 39720cae17e2
 Revises: 
-Create Date: 2023-11-19 13:42:12.706454
+Create Date: 2023-11-18 12:18:30.264542
 
 """
 from alembic import op
@@ -10,7 +10,7 @@ import sqlalchemy as sa
 
 
 # revision identifiers, used by Alembic.
-revision = '9e8969e799bd'
+revision = '39720cae17e2'
 down_revision = None
 branch_labels = None
 depends_on = None
